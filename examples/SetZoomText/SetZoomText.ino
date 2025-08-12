130a598ba87aade66a536be14fe13b92fe5cc5f2
greenonline <greenonline@users.noreply.github.com>
2025-08-12 14:01:35 +0100

    Demo() and tidied

--- examples/SetZoomText/SetZoomText.ino
+++ examples/SetZoomText/SetZoomText.ino
@@ -1,3 +1,5 @@
+// SetZoomText.ino
+
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -15,27 +17,6 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306Plus display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define NUMFLAKES     10 // Number of snowflakes in the animation example
-
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
-static const unsigned char PROGMEM logo_bmp[] =
-{ 0b00000000, 0b11000000,
-  0b00000001, 0b11000000,
-  0b00000001, 0b11000000,
-  0b00000011, 0b11100000,
-  0b11110011, 0b11100000,
-  0b11111110, 0b11111000,
-  0b01111110, 0b11111111,
-  0b00110011, 0b10011111,
-  0b00011111, 0b11111100,
-  0b00001101, 0b01110000,
-  0b00011011, 0b10100000,
-  0b00111111, 0b11100000,
-  0b00111111, 0b11110000,
-  0b01111100, 0b11110000,
-  0b01110000, 0b01110000,
-  0b00000000, 0b00110000 };
 
 void setup() {
   Serial.begin(9600);
@@ -45,11 +26,17 @@ void setup() {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
+  // init done
+
+  // Note: Stop scroll, fade, blink and (especially) zoom.
+  display.stopFadeBlink();
+  display.stopscroll();
+  display.stopZoom();
 
   // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
-  //display.display();
-  //delay(2000); // Pause for 2 seconds
+  display.display();
+  delay(2000); // Pause for 2 seconds
 
   // Clear the buffer
   display.clearDisplay();
@@ -63,18 +50,24 @@ void setup() {
   // drawing operations and then update the screen all at once by calling
   // display.display(). These examples demonstrate both approaches...
 
+  // Call here to run demo one-time only
+  //DoZoomDemo();
 
-  testdrawchar();      // Draw characters of the default font
-
-  //testzoom();
-
-  //testdrawstyles();    // Draw 'stylized' characters
-
-  //testzoom();
 }
 
 void loop() {
+  // Call here to cycle demo indefinitely
+  DoZoomDemo();
+}
+
+void DoZoomDemo(void){
+  testdrawchar();      // Draw characters of the default font
   testzoom();
+  delay(10000);
+
+  testdrawstyles();    // Draw 'stylized' characters
+  testzoom();
+  delay(10000);
 }
 
 void testdrawchar(void) {
