583799258419e0346cd6d3c0508c271691f90fc2
greenonline <greenonline@users.noreply.github.com>
2025-08-13 07:18:40 +0100

    Zoom loop

--- examples/SetZoomText/SetZoomText.ino
+++ examples/SetZoomText/SetZoomText.ino
@@ -8,6 +8,8 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
+#define NUM_ZOOM_LOOP 5
+
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 // The pins for I2C are defined by the Wire-library. 
 // On an arduino UNO:       A4(SDA), A5(SCL)
@@ -61,11 +63,15 @@ void loop() {
 }
 
 void DoZoomDemo(void){
+  int i;
+
   testdrawchar();      // Draw characters of the default font
-  testzoom();
+  for (i=0;i<NUM_ZOOM_LOOP;i++)
+    testzoom();
 
   testdrawstyles();    // Draw 'stylized' characters
-  testzoom();
+  for (i=0;i<NUM_ZOOM_LOOP;i++)
+    testzoom();
 }
 
 void testdrawchar(void) {
